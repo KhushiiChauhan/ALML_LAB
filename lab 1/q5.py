b860267d166488c4523d69bce45ad0af3e1a736b
Khushi Chauhan <142530032+KhushiiChauhan@users.noreply.github.com>
2025-01-13 19:02:55 +0530

    q5

---
+++ lab 1/q5.py
@@ -0,0 +1,11 @@
+# Using a for loop to print numbers from 1 to 10
+print("Numbers using for loop:")
+for i in range(1, 11):
+    print(i)
+
+# Using a while loop to print numbers from 1 to 10
+print("\nNumbers using while loop:")
+counter = 1  # Start from 1
+while counter <= 10:  # Continue until 10
+    print(counter)
+    counter += 1
