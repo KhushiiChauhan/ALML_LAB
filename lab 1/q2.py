1f11a31c55492b03c1b222390bcac702275c71fb
Khushi Chauhan <142530032+KhushiiChauhan@users.noreply.github.com>
2025-01-13 11:45:20 +0530

    q2

---
+++ lab 1/q2.py
@@ -0,0 +1,36 @@
+# Assign values to variables of different data types
+integer = 10
+float_num = 50.0
+string = "Hello "
+boolean = True
+
+# Perform arithmetic operations on numeric data types
+addition = integer + float_num
+subtraction = integer - float_num
+multiplication = integer * float_num
+division = integer / float_num
+modulus = integer % float_num
+
+# Print the results of arithmetic operations
+print("Arithmetic Operations:")
+print(f"Addition: {integer} + {float_num} = {addition}")
+print(f"Subtraction: {integer} - {float_num} = {subtraction}")
+print(f"Multiplication: {integer} * {float_num} = {multiplication}")
+print(f"Division: {integer} / {float_num} = {division}")
+print(f"Modulus: {integer} % {float_num} = {modulus}")
+
+# Concatenate strings using the + operator
+concatenated_string = string + "World!!"
+print("\nString Concatenation:")
+print(concatenated_string)
+
+# Use logical operators to evaluate boolean expressions
+and_op = boolean and False
+or_op = boolean or False
+not_op = not boolean
+
+# Print the results of logical operations
+print("\nLogical Operations:")
+print(f"AND Operation: {boolean} and False = {and_op}")
+print(f"OR Operation: {boolean} or False = {or_op}")
+print(f"NOT Operation: not {boolean} = {not_op}")
